afa058c7e8ba40e3a4ed793aef52858ad8f3a3a7
salvoparisi <psalvo065@gmail.com>
2025-04-03 01:38:08 +0200

    add getChefBirthdate

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-function isRecipe(data: unknown) {
+function isRecipe(data: unknown): boolean {
   if (
     data &&
     typeof data === 'object' &&
-    'userId' in data
+    'userId' in data &&
+    typeof data.userId === 'number'
   ) {
     return true
   } else {
@@ -25,10 +26,37 @@ async function getChefId(id: number): Promise<number | null> {
   }
 }
 
-async function getChefBirthdate(id: number): Promise {
-
+function isChef(data: unknown): boolean {
+  if (
+    data &&
+    typeof data === 'object' &&
+    'birthDate' in data &&
+    typeof data.birthDate === 'string'
+  ) {
+    return true
+  } else {
+    return false
+  }
 }
 
+async function getChefBirthdate(id: number): Promise<string | null> {
+  try {
+    const recipesId = await getChefId(id)
+    const resChef = await fetch(`https://dummyjson.com/users/${recipesId}`)
+    const dataChef = await resChef.json()
+    if (isChef(dataChef)) {
+      console.log(dataChef.birthDate)
+      return dataChef.birthDate
+    } else {
+      throw new Error('Formato non valido')
+    }
+  } catch (err) {
+    console.error(err)
+    return null
+  }
+}
+
+getChefBirthdate(3)
 
 
 
